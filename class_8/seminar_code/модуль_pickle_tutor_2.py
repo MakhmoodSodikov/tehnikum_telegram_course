fcad8b07313f547be6fb6dc8aba2af7124f7a6a6
RomiillST <romiillst@gmail.com>
2021-01-05 20:06:51 +0500

    Update модуль_pickle_tutor_2.py

--- class_8/seminar_code/модуль_pickle_tutor_2.py
+++ class_8/seminar_code/модуль_pickle_tutor_2.py
@@ -8,3 +8,12 @@ print(users_copy)
 # выводим нашу копию в консоль
 file.close()
 # закрываем наш файл
+file = open('data.txt', 'wb')
+
+users_copy = pickle.load(file)
+
+users_copy.append(input('Введите ваше имя: '))
+# добавим имя, которое введёт пользователь. Оно сохранится перманентно
+pickle.dump(users_copy, file)
+print(users_copy)
+file.close()
