a9bdcbeb3269ed326e608f108404584514a94485
RomiillST <romiillst@gmail.com>
2021-01-09 21:47:27 +0500

    Create sql_tutor.py

---
+++ class_10/seminar_code/sql_tutor.py
@@ -0,0 +1,27 @@
+import sqlite3
+# Подключаем модуль базы данных SQL
+
+conn = sqlite3.connect('database.sqlite')
+# Коннектим базу данных 'database.sqlite' к модулю SQL.
+
+cursor = conn.cursor()
+# Указываем на конкретную базу данных, которую мы сохдали и подключили к модулю SQL.
+
+# SQL запрос 'execute' обрабатывает запрос, который получает.
+cursor.execute(''' 
+create table if not exists General_Table ( Имя text,
+                                        Возраст integer, 
+                                        Пол text, 
+                                        Город text,
+                                        Номер телефона text) 
+''') # Создаём таблицу с разными столбцами.
+cursor.execute('''
+insert into General_Table values ('Роман', '19', 'М', 'Ташкент', '+998970042478')
+''') # Добавляем данные в нашу таблицу.
+conn.commit()
+# Подтверждаем добавление.
+print(cursor.execute('''
+select Имя
+from General_Table
+where Город='Ташкент'
+''').fetchall())
