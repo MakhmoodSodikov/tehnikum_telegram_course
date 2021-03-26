dabae48dca4b51f7e83d8948d7a4e56992b13e8d
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-03-26 09:52:55 +0500

    Update answers.py

--- class_work_bot/answers.py
+++ class_work_bot/answers.py
@@ -1,7 +1,7 @@
 import messege as msg
 
 eng_answers = {'Hello': msg.en_hello,
-               'Bye': msg.en_bye,}
+               'Bye': msg.en_bye}
 
 rus_answers = {'привет': msg.ru_hello,
                'пока': msg.ru_bye,
