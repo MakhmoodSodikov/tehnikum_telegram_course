1d2a16a938242170ab3460f07de2bff8954405d3
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-04-06 00:12:49 +0500

    Update main.py

--- class_work_bot/main.py
+++ class_work_bot/main.py
@@ -1,23 +1,32 @@
 """
 Модуль для старта проекта
 """
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
-
-
-
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, Filters
 from const import API_TOKEN
-from function import *
+from functions import *
 
-
-updater = Updater(token=API_TOKEN)
+updater = Updater(token=API_TOKEN, workers=20)
 
 dispatcher = updater.dispatcher
 
-dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler('start',
+                                      start, run_async=True))
 
-dispatcher.add_handler(MessageHandler(Filters.text, text_msg))
+dispatcher.add_handler(MessageHandler(Filters.text,
+                                      text_msg, run_async=True))
 
-dispatcher.add_handler(CallbackQueryHandler(callback=inline_reply, pattern='yes'))
+dispatcher.add_handler(MessageHandler(Filters.contact,
+                                      get_contact))
+
+dispatcher.add_handler(MessageHandler(Filters.location,
+                                      get_location))
+
+dispatcher.add_handler(CallbackQueryHandler(callback=inline_reply,
+                                            pattern='yes'))
+
+dispatcher.add_handler(MessageHandler(Filters.photo,
+                                      photo_msg))
+
 
 updater.start_polling()
 
