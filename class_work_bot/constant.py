a6cc47014f9e94a31d6395163efd3367d1432611
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-04-06 00:44:48 +0500

    Update constant.py

--- class_work_bot/constant.py
+++ class_work_bot/constant.py
@@ -1,4 +1,16 @@
 ""
 Модуль для констант
 """
+
+# TODO: delete tokens before pushing to github
+
 API_TOKEN = '#Сюда ставим актуальный токен созданного бота'
+
+SMS_TOKEN = 'ваш доступ к смс:'
+
+# TODO: reassign path on server
+DB_PATH = 'путь к БД'
+
+SMS_URL = 'ссылка к смс серверу'
+
+MAP_TOKEN = 'токен для локации'
