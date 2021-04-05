63846dc3b0d9d096cc63215282d4682417522f80
Husan-98 <79147345+Husan-98@users.noreply.github.com>
2021-04-06 00:14:06 +0500

    Update markup.py

--- class_work_bot/markup.py
+++ class_work_bot/markup.py
@@ -16,18 +16,49 @@ def build_menu(buttons,
 
 def button():
     button_list = [
-            KeyboardButton("Показать инлайн кнопки"),
-            KeyboardButton("Показать инлайн кнопки"),
-            KeyboardButton("Показать инлайн кнопки"),
-        ]
-    return ReplyKeyboardMarkup(build_menu(button_list, 3))
+            KeyboardButton("Показать инлайн-кнопки"),
+            KeyboardButton("кнопка1"),
+            KeyboardButton("кнопка2"),
+            KeyboardButton("кнопка3"),
+    ]
+
+    return ReplyKeyboardMarkup([[button_list[0], button_list[1], button_list[3]],
+                                [button_list[2], ]])
+
+
+def generate_buttons(item_name, n=6):
+    buttons_list = []
+
+    for i in range(1, n+1):
+        button_caption = str(i)
+        callback_data = item_name + '_' + button_caption
+        buttons_list.append(InlineKeyboardButton(button_caption, callback_data))
+
+    return InlineKeyboardMarkup(build_menu(buttons_list, n // 2))
 
 
 def inline_button():
     button_list = [
-        InlineKeyboardButton("Узнать мнение других!", callback_data='yes'),
-        InlineKeyboardButton("Узнать мнение других!", callback_data='yes'),
-        InlineKeyboardButton("Узнать мнение других!", callback_data='yes'),
-        InlineKeyboardButton("Узнать мнение других!", callback_data='yes'),
+        InlineKeyboardButton("Нажми меня", callback_data='test1234_1'),
+        InlineKeyboardButton("Нажми меня", callback_data='yes'),
+        InlineKeyboardButton("Нажми меня", callback_data='yes'),
+        InlineKeyboardButton("Нажми меня", callback_data='yes'),
     ]
+
     return InlineKeyboardMarkup(build_menu(button_list, 3))
+
+
+def get_phone():
+    button_list = [KeyboardButton("Отправить контакты", request_contact=True)]
+    return ReplyKeyboardMarkup([button_list], resize_keyboard=True)
+
+
+def start_order():
+    button_list = [KeyboardButton("Начать заказ"),
+                   KeyboardButton("История покупок")]
+    return ReplyKeyboardMarkup([button_list], resize_keyboard=True)
+
+
+def get_location():
+    button_list = [KeyboardButton("Отправить локейшн", request_location=True)]
+    return ReplyKeyboardMarkup([button_list], resize_keyboard=True)
